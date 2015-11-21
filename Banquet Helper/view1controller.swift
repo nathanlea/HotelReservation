3df0449e6f68a6b9c1addb911409034f82e8bb51
Nathan <nathan.lea@okstate.edu>
2015-11-21 12:14:42 -0600

    Added Cool Drop downs to project, Sorry that this added Obj-C also :)

--- Banquet Helper/view1controller.swift
+++ Banquet Helper/view1controller.swift
@@ -5,10 +5,11 @@
 //  Created by KASUVAJJALA DEEPTHI  on 10/25/15.
 //  Copyright © 2015 KASUVAJJALA DEEPTHI . All rights reserved.
 //
+// Using this: https://github.com/vinit5320/DropDown-Menu for the Dropdown menus
 
 import UIKit
 //Main View Controller Class
-class ViewController1: UIViewController {
+class ViewController1: UIViewController, IGLDropDownMenuDelegate {
     @IBOutlet weak var ddtext: UILabel!
     @IBOutlet weak var ddMenu: UIView!
     @IBOutlet weak var ddshow: UIButton!
@@ -69,12 +70,76 @@ class ViewController1: UIViewController {
     internal var stateName = ""
     var newColor = UIColor.lightGrayColor().CGColor
     
+    
+    var dropDownMenu = IGLDropDownMenu()
+    
+    var dataImage:NSArray = ["sun.png",
+        "clouds.png",
+        "snow.png" ]
+    var dataTitle:NSArray = ["Buffet",
+        "À la carte",
+        "None" ]
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         textview1.layer.borderColor = newColor
         textview2.layer.borderColor = newColor
         textview1.layer.borderWidth = 1.0
        textview2.layer.borderWidth = 1.0
+        
+        setupInit()
+    }
+    
+    func setupInit() {
+        
+        let dropdownItems:NSMutableArray = NSMutableArray()
+        
+        for i in 0...(dataTitle.count-1) {
+            
+            let item = IGLDropDownItem()
+            item.iconImage = UIImage(named: "\(dataImage[i])")
+            item.text = "\(dataTitle[i])"
+            dropdownItems.addObject(item)
+        }
+        
+        dropDownMenu.menuText = "Choose Food"
+        dropDownMenu.dropDownItems = dropdownItems as [AnyObject]
+        dropDownMenu.paddingLeft = 15
+        dropDownMenu.frame = CGRectMake((self.view.frame.size.width/2) - 100, 150, 200, 45)
+        dropDownMenu.delegate = self
+        dropDownMenu.type = IGLDropDownMenuType.Stack
+        dropDownMenu.gutterY = 5
+        dropDownMenu.itemAnimationDelay = 0.1
+        //dropDownMenu.backgroundColor = UIColor(red: 238, green: 221, blue: 203, alpha: 1)
+        //dropDownMenu.rotate = IGLDropDownMenuRotate.Random //add rotate value for tilting the
+        dropDownMenu.reloadView()
+        
+        let myLabel = UILabel()
+        myLabel.text = "SwiftyOS Blog"
+        myLabel.textColor = UIColor.whiteColor()
+        myLabel.font = UIFont(name: "Helvetica-Neue", size: 17.0)
+        myLabel.textAlignment = NSTextAlignment.Center
+        myLabel.frame = CGRectMake((self.view.frame.size.width/2) - 100, 75, 200, 25)
+        
+        self.view.addSubview(myLabel)
+        self.view.addSubview(self.dropDownMenu)
+        
+    }
+    
+    func dropDownMenu(dropDownMenu: IGLDropDownMenu!, selectedItemAtIndex index: Int) {
+        
+        let item:IGLDropDownItem = dropDownMenu.dropDownItems[index] as! IGLDropDownItem
+        print("Selected weather \(item.text)")
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
